bc7d8f33d00ee16c17b6774ee1fa011fe5d1f9f7
Maria Lopez <trecetreboles@gmail.com>
2023-06-15 12:54:15 +0200

    Contador + ventana

--- app_taxi.py
+++ app_taxi.py
@@ -23,11 +23,8 @@ while True:
    else:
        print('You guessed the number:', secret_number)
        break ''' 
-
-
-
-from http.client import OK
-
+from datetime import datetime
+import tkinter as tk
 
 def mensaje():
     print("Bienvenidos/as al taxiguay.EXPLICACION DE FUNCIONAMIENTO.")
@@ -37,13 +34,44 @@ def mensaje():
         
 mensaje()
 
-
-
-total = OK
-
+total = "ok"
 class Taxi:
     def __init__(self):
-        self.parado = 2
-        self.inicio = self.parado +3
-        self.fin = total
- para los datos
+        self.parado =  True
+        self.inicio = 0
+        self.fin = 0
+
+
+
+
+#Cronómetro y ventana emergente que nos muestra el tiempo que ha transcurrido desde el inicio. 
+
+hora_inicio = datetime.now()
+intervalos = 500 
+
+def segundos_total(segundos):
+    horas = int(segundos /60/60)
+    segundos -= horas *60*60
+    minutos= int (segundos/60)
+    segundos -= minutos*60
+    return f"{horas:02d}:{minutos:02d}:{segundos:02d}"
+
+def t_formateado ():
+    seg_transcurridos= (datetime.now()- hora_inicio).total_seconds()
+    return segundos_total(int(seg_transcurridos))
+
+def refresh_tiempo():
+    print("funciona")
+    v_hora_actual.set(t_formateado())
+    raiz.after(intervalos, refresh_tiempo)
+
+raiz= tk.Tk()
+v_hora_actual = tk.StringVar(raiz, value = t_formateado())
+raiz.etiqueta= tk.Label(
+    raiz, textvariable=v_hora_actual, font=f"Consolas 60")
+raiz.etiqueta.pack(side="top")
+app = tk.Frame()
+raiz.title("prueba")
+refresh_tiempo()
+app.pack()
+app.mainloop()
