b507768c5a9e250d233913c0258b509ee1720ecd
Victoria-moraleda <victoriamoraledablancoi@gmail.com>
2023-06-20 10:05:01 +0200

    pausas creadas, pero sin detener tiempo

--- taxi_OK.py
+++ taxi_OK.py
@@ -1,3 +1,4 @@
+#FUNCIONA, NOS DICE PAUSAS PERO NO DETIENE EL CONTADOR
 import time
 
 class Cronometro:
@@ -20,19 +21,17 @@ class Cronometro:
         if self.en_ejecucion:
             self.tiempo_pausado = time.time() - self.inicio
             #calcula el tiempo que pasa desde que inicia hasta la pausa
-            print("El cronómetro sigue en marcha.")
+            print("El cronómetro está pausado.")
 
     def reanudar(self):
         if not self.en_ejecucion:
             self.inicio = time.time() - self.tiempo_pausado
-            #valor actual menos tiempo pausado, se hace parar tener en cuenta el tiempo que ha pasado durante la pause y ajustar el tiempo de inicio
             self.en_ejecucion = True
-            print("Cronómetro continúa.")
+            print("Cronómetro reanudado.")
 
     def finalizar(self):
         if self.en_ejecucion:
             self.tiempo_transcurrido = time.time() - self.inicio
-            #tiempo total transcurrido desde el inicio del crono hasta el final
             self.en_ejecucion = False
             print("Cronómetro finalizado.")
         print(f"Tiempo total transcurrido: {self.tiempo_transcurrido} segundos.")
@@ -40,6 +39,100 @@ class Cronometro:
 class Taxi:
     def __init__(self):
         self.cronometro = Cronometro()
+        #cada objeto taxi tendrá su propio obj crono asociado para restrear el tiempo
+        self.estado = "parado"
+        self.tiempo_pausado = 0
+
+    def empezar(self):
+        if self.estado == "parado":
+            self.cronometro.empezar()
+            self.estado = "en marcha"
+            print("El taxi está en marcha.")
+
+    def parar(self):
+        if self.estado == "en marcha":
+            self.cronometro.parar()
+            self.estado = "parado"
+            print("El taxi está parado.")
+            self.tiempo_pausado = time.time()
+
+    def reanudar(self):
+        if self.estado == "parado":
+            self.cronometro.reanudar()
+            self.estado = "en marcha"
+            tiempo_pausa = time.time() - self.tiempo_pausado
+            #valor actual menos tiempo pausado, se hace parar tener en cuenta el tiempo que ha pasado durante la pause y ajustar el tiempo de inicio
+            print(f"El taxi ha sido reanudado. Tiempo de pausa: {tiempo_pausa} segundos.")
+
+    def finalizar(self):
+        self.cronometro.finalizar()
+        self.estado = "finalizado"
+        print("El taxi ha finalizado su servicio.")
+
+# Ejemplo de uso
+taxi = Taxi()
+
+while True:
+    comando = input("Introduce un comando (empezar, parar, reanudar, finalizar): ")
+
+    if comando == "empezar":
+        taxi.empezar()
+    elif comando == "parar":
+        taxi.parar()
+    elif comando == "reanudar":
+        taxi.reanudar()
+    elif comando == "finalizar":
+        taxi.finalizar()
+        break
+    else:
+        print("Comando inválido.")
+
+'''
+SIN PAUSA
+
+import time
+
+class Cronometro:
+    def __init__(self):
+        self.inicio = 0
+        self.tiempo_pausado = 0
+        self.tiempo_transcurrido = 0
+        self.en_ejecucion = False
+        
+
+    def empezar(self):
+        if not self.en_ejecucion:
+            self.inicio = time.time() - self.tiempo_pausado
+            
+            self.en_ejecucion = True
+            
+            print("Cronómetro empezado.")
+
+    def parar(self):
+        if self.en_ejecucion:
+            self.tiempo_pausado = time.time() - self.inicio
+            
+            print("El cronómetro sigue en marcha.")
+
+    def reanudar(self):
+        if not self.en_ejecucion:
+            self.inicio = time.time() - self.tiempo_pausado
+            
+            self.en_ejecucion = True
+            print("Cronómetro continúa.")
+
+    def finalizar(self):
+        if self.en_ejecucion:
+            self.tiempo_transcurrido = time.time() - self.inicio
+            
+            self.en_ejecucion = False
+            print("Cronómetro finalizado.")
+        print(f"Tiempo total transcurrido: {self.tiempo_transcurrido} segundos.")
+
+class Taxi:
+    def __init__(self):
+        self.cronometro = Cronometro()
+        
         self.estado = "parado"
 
     def empezar(self):
@@ -82,3 +175,7 @@ while True:
         break
     else:
         print("Comando inválido.")
+        '''
+
+
+
